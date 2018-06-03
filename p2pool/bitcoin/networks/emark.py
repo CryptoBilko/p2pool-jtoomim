63558d985f952a63fe2160317de2c6a04ffc57e2
CryptoBilko <s.sieweke@gmx.de>
2018-06-03 16:42:20 +0200

    Update emark.py

--- p2pool/bitcoin/networks/emark.py
+++ p2pool/bitcoin/networks/emark.py
@@ -21,9 +21,9 @@ POW_FUNC = data.hash256
 BLOCK_PERIOD = 600 # s
 SYMBOL = 'DEM'
 CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Bitcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Bitcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.eMark'), 'eMark.conf')
-BLOCK_EXPLORER_URL_PREFIX = 'https://blockchair.com/bitcoin-cash/block/'
-ADDRESS_EXPLORER_URL_PREFIX = 'https://blockchair.com/bitcoin-cash/address/'
-TX_EXPLORER_URL_PREFIX = 'https://blockchair.com/bitcoin-cash/transaction/'
+BLOCK_EXPLORER_URL_PREFIX = 'http://185.194.142.165:3001/block/'
+ADDRESS_EXPLORER_URL_PREFIX = 'http://185.194.142.165:3001/ext/getaddress/'
+TX_EXPLORER_URL_PREFIX = 'http://185.194.142.165:3001/tx/'
 SANE_TARGET_RANGE = (2**256//2**32//1000000 - 1, 2**256//2**32 - 1)
 DUMB_SCRYPT_DIFF = 1
 DUST_THRESHOLD = 0.001e8
