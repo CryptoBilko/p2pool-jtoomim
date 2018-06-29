8fba07e375c6af5a9350db98be99ed4038141eb5
CryptoBilko <s.sieweke@gmx.de>
2018-06-29 10:43:35 +0200

    Update emark.py

--- p2pool/networks/emark.py
+++ p2pool/networks/emark.py
@@ -6,22 +6,22 @@ from p2pool.bitcoin import networks
 # REAL_CHAIN_LENGTH must be changed in sync with all other clients
 # changes can be done by changing one, then the other
 
-PARENT = networks.nets['bitcoin']
-SHARE_PERIOD = 30 # seconds
-CHAIN_LENGTH = 24*60*60//10 # shares
-REAL_CHAIN_LENGTH = 24*60*60//10 # shares
-TARGET_LOOKBEHIND = 200 # shares
-SPREAD = 3 # blocks
-IDENTIFIER = 'fc70035c7a81bc6f'.decode('hex')
-PREFIX = '2472ef181efcd37b'.decode('hex')
-P2P_PORT = 9335
+PARENT = networks.nets['emark']
+SHARE_PERIOD = 24 # seconds
+CHAIN_LENGTH = 7*24*60*60//10 # shares
+REAL_CHAIN_LENGTH = 7*24*60*60//10 # shares
+TARGET_LOOKBEHIND = 20 # shares
+SPREAD = 15 # blocks
+IDENTIFIER = '6012865EBAD2C803'.decode('hex') #654d61726b
+PREFIX = ''.decode('hex')
+P2P_PORT = 5556
 MIN_TARGET = 0
 MAX_TARGET = 2**256//2**32 - 1
-PERSIST = True
-WORKER_PORT = 9334
-BOOTSTRAP_ADDRS = 'ml.toom.im ml.toom.im:9336 ml.toom.im:9334 93.77.99.166 whortonda.noip.me:9333 btc-fork.coinpool.pw:9335 crypto.office-on-the.net:9335 siberia.mine.nu:9335'.split(' ')
-ANNOUNCE_CHANNEL = '#p2pool'
-VERSION_CHECK = lambda v: None if 100000 <= v else 'Bitcoin version too old. Upgrade to 0.11.2 or newer!' # not a bug. BIP65 support is ensured by SOFTFORKS_REQUIRED
+PERSIST = False
+WORKER_PORT = 6667
+BOOTSTRAP_ADDRS = 'poolmining.net'.split(' ')
+ANNOUNCE_CHANNEL = '#p2pool-alt'
+VERSION_CHECK = lambda v: None if 60007 <= v else 'eMark version too old. Upgrade to 1.1.0.0 or newer!' # not a bug. BIP65 support is ensured by SOFTFORKS_REQUIRED
 VERSION_WARNING = lambda v: None
 SOFTFORKS_REQUIRED = set(['bip65', 'csv', 'segwit'])
 MINIMUM_PROTOCOL_VERSION = 3301
